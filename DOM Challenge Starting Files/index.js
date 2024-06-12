f75f1c2a098d8f1bfb112718fce7b871fdbd73cf
Lorenzo Locker <lo.locker1202@gmail.com>
2024-06-11 23:06:18 -0300

    feat: Added DOM and DOM Challenge

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelector("ul").lastElementChild.innerHTML = "Lorenzo";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("button").classList.toggle("invisible");
