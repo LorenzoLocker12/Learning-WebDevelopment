7b67e478e54190b72f1b1e51dd06548052756309
Lorenzo Locker <lo.locker1202@gmail.com>
2024-06-14 00:30:08 -0300

    Added Drum Kit and JQUery Lessons

---
+++ jQuery/index.js
@@ -0,0 +1,31 @@
+
+
+$("h1").toggleClass("big-title margin-50")
+
+$("button").html("<em>I Love jQuery<em>")
+
+
+
+
+
+
+
+$("button").click(function(){
+
+    if ($("h1").hasClass("big-title")) {
+
+        $("h1").removeClass("big-title");
+    }
+
+});
+
+    
+
+ $("h1").on("mouseover", function(){
+
+    // $("h1").fadeOut();
+    // $("h1").show();
+    // $("h1").slideUp();
+    $("h1").animate({opacity: })
+
+ });
