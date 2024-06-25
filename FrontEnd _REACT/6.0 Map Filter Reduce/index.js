cc543848541c3c4da3f50a2941fa482001a425ed
Lorenzo Locker <lo.locker1202@gmail.com>
2024-06-25 01:30:14 -0300

    feat: Added lessons 5.0 and 6.0

---
+++ FrontEnd _REACT/6.0 Map Filter Reduce/index.js
@@ -0,0 +1,50 @@
+import emojipedia from "./emojipedia";
+
+var numbers = [3, 56, 2, 48, 5];
+
+// MAP
+
+// const newNumbers = numbers.map(function (x){
+//     return x * 2;
+// });
+
+
+//FOR EACH
+
+// var newNumbers = [];
+// numbers.forEach(function (x) {
+//     ewNumbers.push(x * 2);
+// });
+
+
+//FILTER
+
+// const newNumbers = numbers.filter(function (x){
+//     return x > 10;
+// });
+
+
+//REDUCE
+
+// const newNumbers = numbers.reduce(function (accumulator, currentNumber){
+//    console.log("accumulator = " + accumulator);
+//    console.log("currentNumber = " + currentNumber);
+//    return accumulator + currentNumber; 
+// });
+
+
+//FIND
+
+// const newNumber = numbers.find(function (x){
+//     return x > 10;
+// });
+
+
+//FIND INDEX
+
+// const newNumber = numbers.findIndex(function (x){
+//     return x > 10;
+// });
+
+console.log(newNumbers);
+
